e89a625f7d9312888f524886690c4569058969e3
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-22 16:15:38 +0100

    Initial Commit

---
+++ Hyke/HykeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HykeApp.swift
+//  Hyke
+//
+//  Created by Carlo Contardi on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HykeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
