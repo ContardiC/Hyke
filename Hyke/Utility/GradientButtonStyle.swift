e8d67a767bc2a5a9beaf807f3734cc9bc1905bd0
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-23 09:34:36 +0100

    feat: improve UX for the CustomGradientButton

--- Hyke/Utility/GradientButtonStyle.swift
+++ Hyke/Utility/GradientButtonStyle.swift
@@ -3,6 +3,27 @@
 //  Hyke
 //
 //  Created by Carlo Contardi on 23/03/25.
-//
+//  https://www.spacecoding.it
 
 import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional Statement with Ternary Conditional Operator
+                // Consition ? A : B
+                configuration.isPressed ?
+                // A: When user pressed the Button
+                LinearGradient(colors: [.customGrayMedium, .customGrayLight], startPoint: .top, endPoint: .bottom)
+                // B: When the Button is not pressed
+                :
+                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+            )
+            .cornerRadius(40)
+    }
+}
