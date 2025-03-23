4dbe0192919a478257d29a756b2405e18503691b
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-23 16:11:47 +0100

    feat: add Gradient Animation to the CustomCircleView

---
+++ Hyke/Component/CustomCircleView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomCircleView.swift
+//  Hyke
+//
+//  Created by Carlo Contardi on 23/03/25.
+//  https://www.spacecoding.it
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    
+    @State private var isAnimateGradient: Bool = false
+    var body: some View {
+        Circle()
+        .fill(
+            LinearGradient(colors: [
+                
+                .customIndigoMedium, .customSalmonLight],
+                           startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                           endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing)
+        )
+        .onAppear{
+            withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)){
+                isAnimateGradient.toggle()
+            }
+        }
+        .frame(width: 256, height: 256)
+        
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
