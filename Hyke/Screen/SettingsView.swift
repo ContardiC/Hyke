86dfee4066002268214e3786605411d1802b87f5
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-24 17:25:03 +0100

    feat: add CustomListRowView

--- Hyke/Screen/SettingsView.swift
+++ Hyke/Screen/SettingsView.swift
@@ -59,27 +59,10 @@ struct SettingsView: View {
                 
             ){
                 // 1. Basic Labeled Content
-                LabeledContent("Application", value: "Hike")
+                // LabeledContent("Application", value: "Hike")
+                
                 // 2. Advaced Labeled Content
-                LabeledContent{
-                    // Content
-                    Text("Hike")
-                        .foregroundColor(.primary)
-                        .fontWeight(.heavy)
-                }label: {
-                    HStack {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 8)
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(.blue)
-                            Image(systemName: "apps.iphone")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                            
-                        }
-                        Text("Application")
-                    }
-                }
+                
             }//: SECTION
         }//: LIST
         
